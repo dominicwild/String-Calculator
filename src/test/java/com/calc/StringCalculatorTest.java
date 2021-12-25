fc6bb53362d2b194e5fd38eec91e5446ea7cfe64
dwild8 <dwild8@dxc.com>
2021-12-25 03:34:29 +0000

    Initial test

--- src/test/java/com/calc/StringCalculatorTest.java
+++ src/test/java/com/calc/StringCalculatorTest.java
@@ -1,7 +1,20 @@
 package com.calc;
 
 import org.junit.jupiter.api.Test;
+import org.quicktheories.WithQuickTheories;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.quicktheories.QuickTheory.qt;
+import static org.quicktheories.generators.SourceDSL.*;
+import static org.quicktheories.generators.Generate.*;
 
 public class StringCalculatorTest {
+
+	@Test
+	void empty_string_returns_zero() {
+		int zero = 0;
+		String blankString = "";
+		assertEquals(zero, StringCalculator.add(blankString));
+	}
 
 }
