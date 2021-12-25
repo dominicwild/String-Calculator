c7d7b6f3a85965b214f01733b3baa011053948c0
dwild8 <dwild8@dxc.com>
2021-12-25 23:08:21 +0000

    Allow custom one character delimiters

--- src/main/java/com/calc/StringCalculator.java
+++ src/main/java/com/calc/StringCalculator.java
@@ -3,11 +3,22 @@ package com.calc;
 import java.util.stream.Stream;
 
 class StringCalculator {
+
+	private static final String CUSTOM_DELIMITER_PREFIX = "//";
+
 	public static int add(String numbers) {
 		if (numbers.equals("")) {
 			return 0;
 		}
 
-		return Stream.of(numbers.split("(,|\\\\n)")).map(Integer::parseInt).reduce(0, Integer::sum);
+		String splitRegex = "(,|\\\\n)";
+
+		if (numbers.startsWith(CUSTOM_DELIMITER_PREFIX)) {
+			String customDelimiter = numbers.charAt(2) + "";
+			splitRegex = String.format("\\Q%s\\E", customDelimiter);
+			numbers = numbers.substring(5);
+		}
+
+		return Stream.of(numbers.split(splitRegex)).map(Integer::parseInt).reduce(0, Integer::sum);
 	}
 }
--- src/test/java/com/calc/StringCalculatorTest.java
+++ src/test/java/com/calc/StringCalculatorTest.java
@@ -3,7 +3,7 @@ package com.calc;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.quicktheories.generators.Generate;
+import org.quicktheories.generators.SourceDSL;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.quicktheories.QuickTheory.qt;
@@ -40,7 +40,7 @@ public class StringCalculatorTest {
 	@ValueSource(strings = { ",", "\\n" })
 	void separated_numbers_returns_sum(String delimiter) {
 		qt()
-				.forAll(Generate.intArrays(range(0, 1000), range(0, 10_000)))
+				.forAll(intArrays(range(0, 1000), range(0, 10_000)))
 				.check((numbers) -> {
 					StringJoiner numberJoin = new StringJoiner(delimiter);
 					int sumOfNumbers = 0;
@@ -55,12 +55,29 @@ public class StringCalculatorTest {
 	@Test
 	void numbers_with_mixed_separators_return_sum() {
 		qt()
-				.forAll(Generate.intArrays(range(0, 1000), range(0, 10_000)))
+				.forAll(intArrays(range(0, 1000), range(0, 10_000)))
 				.check((numbers) -> {
 					StringBuilder stringBuilder = new StringBuilder();
 					int sumOfNumbers = 0;
 					for (int number : numbers) {
 						stringBuilder.append(number + randomDelimiter());
+						sumOfNumbers += number;
+					}
+					return StringCalculator.add(stringBuilder.toString()) == sumOfNumbers;
+				});
+	}
+
+	@Test
+	void numbers_with_custom_separator_return_sum() {
+		qt()
+				.forAll(intArrays(range(1, 1000), range(0, 10_000)),
+						strings().ascii().ofLength(1).assuming(string -> !string.isBlank() && !string.matches("[0-9]")))
+				.check((numbers, delimiter) -> {
+					StringBuilder stringBuilder = new StringBuilder();
+					stringBuilder.append("//" + delimiter + "\\n");
+					int sumOfNumbers = 0;
+					for (int number : numbers) {
+						stringBuilder.append(number + delimiter);
 						sumOfNumbers += number;
 					}
 					return StringCalculator.add(stringBuilder.toString()) == sumOfNumbers;
