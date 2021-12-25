6b8e10c08bf10506d22bf75bd0f0acaef2e1f012
dwild8 <dwild8@dxc.com>
2021-12-25 04:12:12 +0000

    Expanded test to accomodate string of any length

--- src/test/java/com/calc/StringCalculatorTest.java
+++ src/test/java/com/calc/StringCalculatorTest.java
@@ -1,10 +1,14 @@
 package com.calc;
 
 import org.junit.jupiter.api.Test;
+import org.quicktheories.generators.Generate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.quicktheories.QuickTheory.qt;
 import static org.quicktheories.generators.SourceDSL.*;
+
+import java.util.StringJoiner;
+
 import static org.quicktheories.generators.Generate.*;
 
 public class StringCalculatorTest {
@@ -26,8 +30,24 @@ public class StringCalculatorTest {
 	@Test
 	void two_numbers_returns_sum() {
 		qt()
-				.forAll(range(0, Integer.MAX_VALUE), range(0, Integer.MAX_VALUE))
+				.forAll(range(0, Integer.MAX_VALUE / 2), range(0, Integer.MAX_VALUE / 2))
 				.check((num1, num2) -> StringCalculator.add(num1 + "," + num2) == num1 + num2);
+	}
+
+	@Test
+
+	void comma_separated_numbers_returns_sum() {
+		qt()
+				.forAll(Generate.intArrays(range(0, 100_000), range(0, 10_000)))
+				.check((numbers) -> {
+					StringJoiner numberJoin = new StringJoiner(",");
+					int sumOfNumbers = 0;
+					for(int number : numbers) {
+						numberJoin.add(number + "");
+						sumOfNumbers += number;
+					}
+					return StringCalculator.add(numberJoin.toString()) == sumOfNumbers;
+				});
 	}
 
 }
